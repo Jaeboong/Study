e1d8922ee1c9c7343286e6b8e70159e8d28119d6
Jaykim <cbkjh0225@gmail.com>
2024-03-12 21:41:42 +0900

    UDR 챌린지 2일차

---
+++ UDR_챌린지/UDR_챌린지_2일차/lecture07.js
@@ -0,0 +1,8 @@
+var empty_obj = {};
+console.log(typeof(empty_obj));
+
+var man = {name: "Hong gil dong", age: 20, height:188};
+console.log(man.name);
+console.log(man.age);
+console.log(man["height"]);
+console.log(man);
---
+++ UDR_챌린지/UDR_챌린지_2일차/lecture08.js
@@ -0,0 +1,9 @@
+var uninitialized_var;
+console.log("print uninitialized_var: ", uninitialized_var);
+console.log("print uninitialized_var type: ", typeof(uninitialized_var));
+
+var obj = {x:1, y:2};
+console.log("print obj.z: ", obj.z);
+
+var null_var = null;
+console.log("print null_var type: ", typeof(null_var));
