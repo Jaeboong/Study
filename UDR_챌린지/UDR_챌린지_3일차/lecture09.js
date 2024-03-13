c6aea9ad635376c7c247795b5bf52829317ad052
Jaykim <cbkjh0225@gmail.com>
2024-03-13 21:00:14 +0900

    UDR 챌린지 3일차

---
+++ UDR_챌린지/UDR_챌린지_3일차/lecture09.js
@@ -0,0 +1,14 @@
+a = 4, b = 5;
+
+console.log("a:", a, " b:", b);
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);
+
+console.log("a++=",a++);
+console.log("a--=",b--);
+
+console.log("a^2=",Math.pow(a, b));
+console.log("sqrt(a)=",Math.sqrt(b));
---
+++ UDR_챌린지/UDR_챌린지_3일차/lecture12.js
@@ -0,0 +1,12 @@
+str1 = "Hello ", str2 = "javascript ", str3 = "is fun!";
+num1 = 123, num2 = 456;
+
+console.log(str1.concat(str2));
+console.log(str1.concat(str2).concat(str3));
+
+console.log(str1+str2+str3);
+console.log(str1+num1);
+console.log(str1+num1+str2+num2);
+
+console.log(str1.length);
+console.log((str1+str2).length);
