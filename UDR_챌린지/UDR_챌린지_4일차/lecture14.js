c6573af4d463092f003541780942f25eafb046c4
Jaykim <cbkjh0225@gmail.com>
2024-03-15 23:10:19 +0900

    UDR 챌린지 4일차

---
+++ UDR_챌린지/UDR_챌린지_4일차/lecture14.js
@@ -0,0 +1,15 @@
+var arr = [];
+
+var arr2= [1,2,3,4,5];
+
+console.log(arr2[3]);
+
+var mixed_arr =[1, true, 3.14, "string", {name:"object"}];
+
+console.log(mixed_arr[0]);
+console.log(mixed_arr[1]);
+console.log(mixed_arr[2]);
+console.log(mixed_arr[3]);
+console.log(mixed_arr[4]);
+console.log(mixed_arr[7]);
+console.log(mixed_arr.length);
---
+++ UDR_챌린지/UDR_챌린지_4일차/lecture16.js
@@ -0,0 +1,24 @@
+arr1 = [1,2,3], arr2 = [4,5,6]; // 주석주석
+
+console.log(arr1.sort(), arr2.sort()); //주석주석주석
+
+arr1.push(4);
+arr2.pop();
+arr1.shift();
+arr1.unshift(0);
+arr1.reverse();
+
+console.log(arr1.sort(), arr2.sort());
+
+/*
+주
+arr1.concat(arr2);
+console.log(arr1.indexOf(2));
+console.log(arr1.lastIndexOf(4));
+
+var str = "1,2,3,4,5";
+arr = str.split(",");
+
+console.log(arr.sort());
+석
+*/
